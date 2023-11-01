f9da6ebc0fc95ae21942a0471399587a9678f967
mahmud-alam <official.mahmud23@gmail.com>
2023-11-01 22:17:51 +0600

    image drag and drop worked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import l from './data/img/image-1.webp'
+import { data } from "./data/data";
+import { closestCenter, DndContext } from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
-function App() {
+const SortableImg = ({ img }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: img.id });
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="img"
+    >
+      <img src={`${img.path}`} alt=""/>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [images, setImages] = useState(data);
+  const [inputValue, setInputValue] = useState("");
+  const addImg = () => {
+    const newImg = {
+      id: Date.now().toString(),
+      name: inputValue,
+    };
+    setInputValue("");
+    setImages((images) => [...images, newImg]);
+  };
+
+  const onDragEnd = (event) => {
+    const { active, over } = event;
+    if (active.id === over.id) {
+      return;
+    }
+    setImages((images) => {
+      const oldIndex = images.findIndex((img) => img.id === active.id);
+      const newIndex = images.findIndex((img) => img.id === over.id);
+      return arrayMove(images, oldIndex, newIndex);
+    });
+  };
+
+  return (
+    <div className="images">
+      <div>Total: {images.length}</div>
+      <div className="images-form">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={addImg}>Add Image</button>
+      </div>
+      <div>
+        <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+          <SortableContext items={images} strategy={verticalListSortingStrategy}>
+            {images.map((img) => (
+              <SortableImg key={img.id} img={img} />
+            ))}
+          </SortableContext>
+        </DndContext>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/data/data.js
@@ -0,0 +1,46 @@
+export const data = [
+  {
+    id: 1,
+    path: "./data/img/image-1.webp"
+  },
+  {
+    id: 2,
+    path: "./data/img/image-2.webp"
+  },
+  {
+    id: 3,
+    path: "./data/img/image-3.webp"
+  },
+  {
+    id: 4,
+    path: "./data/img/image-4.webp"
+  },
+  {
+    id: 5,
+    path: "./data/img/image-5.webp"
+  },
+  {
+    id: 6,
+    path: "./data/img/image-6.webp"
+  },
+  {
+    id: 7,
+    path: "./data/img/image-7.webp"
+  },
+  {
+    id: 8,
+    path: "./data/img/image-8.webp"
+  },
+  {
+    id: 9,
+    path: "./data/img/image-9.webp"
+  },
+  {
+    id: 10,
+    path: "./data/img/image-10.jpeg"
+  },
+  {
+    id: 11,
+    path: "./data/img/image-11.jpeg"
+  },
+];
