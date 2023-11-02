bbd16fb901e0a9295d5122a4b05efe5d113a66a4
mahmud-alam <official.mahmud23@gmail.com>
2023-11-02 21:11:37 +0600

    Project Done

--- src/App.js
+++ src/App.js
@@ -2,33 +2,30 @@ import "./App.css";
 import React, { useState } from "react";
 import { data } from "./data/data";
 import { closestCenter, DndContext } from "@dnd-kit/core";
-import {
-  arrayMove,
-  SortableContext,
-  useSortable,
-  verticalListSortingStrategy,
-} from "@dnd-kit/sortable";
+import { arrayMove, SortableContext, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
-const SortableImg = ({ img }) => {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: img.id });
+const SortableImg = ({ img, firstImg }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: img.id });
   const style = {
     transition,
     transform: CSS.Transform.toString(transform),
   };
 
+  let cName = "";
+  if (img.id === firstImg) {
+    cName = "firstImg-size";
+  } else {
+    cName = "img-size";
+  }
+
   return (
-    <div
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-      className="img"
-    >
-      {img.name}
-      <img src={img.path} alt="" />
-    </div>
+    <section ref={setNodeRef} style={style} {...attributes} {...listeners} className={cName}>
+      <div className="img-div">
+        <img src={img.path} alt="" />
+        <input className="img-checkbox" type="checkbox" name="checkbox" id="checkbox"/>
+      </div>
+    </section>
   );
 };
 
@@ -57,34 +54,29 @@ const App = () => {
     });
   };
 
-  return (
-    <div className="images">
-      <div>Total: {images.length}</div>
+  let firstImg = images[0].id;
 
-      <div>
+  return (
+    <main className="container">
+      <div className="text-div">Total Images: {images.length}</div>
+      <div className="img-grid-section">
         <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
-          <SortableContext
-            items={images}
-            strategy={verticalListSortingStrategy}
-          >
-            {images.map((img) => (
-              <SortableImg key={img.id} img={img} />
-            ))}
+          <SortableContext items={images} strategy={verticalListSortingStrategy}>
+            {
+              images.map((img) => (
+                <SortableImg key={img.id} img={img} firstImg={firstImg} />
+              ))
+            }
           </SortableContext>
         </DndContext>
-      </div>
 
-      <label className="add-images-label">
-        Add Images
-        <input
-          className="add-images-input"
-          type="file"
-          name="images"
-          onChange={addImg}
-          accept="image/png, image/jpeg"
-        />
-      </label>
-    </div>
+        <label className="add-images-label img-size">
+          <img src="img/icon.jpg" alt="" />
+          Add Images
+          <input className="add-images-input" type="file" name="images" onChange={addImg} accept="image/png, image/jpeg"/>
+        </label>
+      </div>
+    </main>
   );
 };
 
